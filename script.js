e39be9615047cc29187d015099935d2f09f80ada
giodude23 <nlbugtong@gmail.com>
2021-04-12 15:17:05 +0800

    Add div with background and border styles, add h1 and p elements inside div

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ const blueH3 = document.createElement("h3");
 blueH3.style.color = "blue";
 blueH3.textContent = "I'm a blue h3";
 container.appendChild(blueH3);
+
+const div = document.querySelector("div");
+div.style.cssText = "border: 1px solid black; background: pink;)";
+container.appendChild(div);
+
+const h1 = document.querySelector("h1");
+h1.textContent = "I'm in a div";
+div.appendChild(h1);
+
+const p = document.querySelector("p");
+p.textContent = "ME TOO!";
+div.appendChild(p);
+
